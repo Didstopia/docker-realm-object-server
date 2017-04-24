7d271c322cc9703a41ab97cd4d4d7da00a4d4c0c
Pauli Jokela <pauli.jokela@didstopia.com>
2017-04-24 12:41:20 +0300

    Disabled pushing to Docker Hub when the tag already exists

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,7 +8,10 @@ set -o pipefail
 cd "${0%/*}"/../
 
 # Create and push a new tag for the current version
-./scripts/tag.sh
+if ./scripts/tag.sh | grep -q 'already exists'; then
+	echo "Tag already exists, skipping push to Docker Hub.."
+	exit 0
+fi
 
-# Push the new version for Docker Cloud
+# Push the new version for Docker Hub
 ./scripts/docker_push.sh
